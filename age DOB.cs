eafd0ea614b65aad5481bb3cca1df45089a8c333
unknown <diviyani.vel@gmail.com>
2021-04-28 17:53:51 +0530

    first commit

---
+++ age DOB.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		DateTime dob=new DateTime(1999,6,07);
+		DateTime dt=DateTime.Now;
+		int Day= dt.Day-dob.Day;
+		int Month= dt.Month-dob.Month;
+		int Year= dt.Year-dob.Year;
+		Console.WriteLine(String.Format("Age: " +Year+ " Year(s) " +Month+" Month(s) " +Day+" Day(s) "));
+	}
+}
